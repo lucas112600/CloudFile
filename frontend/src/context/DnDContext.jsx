84079793828196309a7f542fdea39675f132ed56
lucas112600 <huchialun971126@gmail.com>
2026-04-23 10:46:30 +0800

    Add frontend/src/context/DnDContext.jsx

---
+++ frontend/src/context/DnDContext.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const DnDContext = createContext([null, () => {}]);
+
+export const DnDProvider = ({ children }) => {
+  const [type, setType] = useState(null);
+
+  return (
+    <DnDContext.Provider value={[type, setType]}>
+      {children}
+    </DnDContext.Provider>
+  );
+};
+
+export default DnDContext;
